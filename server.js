81869bd71110761b445a58f347baf4c81eb071fa
thientran2606 <hieuthien499@gmail.com>
2024-02-24 14:52:17 +0700

    cai dat mongodb

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const Mongodb = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+async function startServer() {
+    try {
+        await Mongodb.connect(config.db.uri);
+        console.log("Conncected to the database!");
+
+        const PORT = config.app.port;
+        app.listen(PORT,() => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log("Cannot connect to the database!",error);
+        process.exit();
+    }
+}
+
+startServer();
