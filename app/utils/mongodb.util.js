ab6bdf2bf376e4e0d7181294b365a4ab08cc8d55
thientran2606 <hieuthien499@gmail.com>
2024-02-26 10:05:50 +0700

    Cai dat cac handler truy xuat CSDL

--- app/utils/mongodb.util.js
+++ app/utils/mongodb.util.js
@@ -1,6 +1,6 @@
-const { MongoClient } = require("mongodb");
+const {MongoClient} = require("mongodb");
 
-class Mongodb {
+class MongoDB {
     static connect = async (uri) => {
         if(this.client) return this.client;
         this.client = await MongoClient.connect(uri);
@@ -8,4 +8,4 @@ class Mongodb {
     };
 }
 
-module.exports = Mongodb;
+module.exports = MongoDB;
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const app = require("./app");
 const config = require("./app/config");
-const Mongodb = require("./app/utils/mongodb.util");
+const MongoDB = require("./app/utils/mongodb.util");
 
 async function startServer() {
     try {
-        await Mongodb.connect(config.db.uri);
-        console.log("Conncected to the database!");
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
 
         const PORT = config.app.port;
-        app.listen(PORT,() => {
+        app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
         console.log("Cannot connect to the database!",error);
-        process.exit();
     }
 }
 
